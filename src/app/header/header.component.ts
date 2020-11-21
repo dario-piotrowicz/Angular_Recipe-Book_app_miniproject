ea8566e2e72c680e5c3efcac8133b56c9f94c4a7
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-11-21 21:54:55 +0100

    added nav-toggler to header component (for smaller screens)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  public navbarCollapsed = true;
+  private resizeSubscription: Subscription = null;
+
+  ngOnInit(): void {
+    this.resizeSubscription = fromEvent(window, 'resize').subscribe(
+      () => (this.navbarCollapsed = true)
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
+  }
 }
