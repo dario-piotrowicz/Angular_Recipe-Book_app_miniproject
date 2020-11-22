ed6367021a19fd5c5402761d9c7adc92a74ffd97
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-11-22 10:56:13 +0100

    collapsing the menu on resize in header component in a cleaner way (no rxjs)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { fromEvent, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent {
   public navbarCollapsed = true;
-  private resizeSubscription: Subscription = null;
-
-  ngOnInit(): void {
-    this.resizeSubscription = fromEvent(window, 'resize').subscribe(
-      () => (this.navbarCollapsed = true)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.resizeSubscription) {
-      this.resizeSubscription.unsubscribe();
-    }
-  }
 }
