5851e34fb356a5265653adef98bf3c1ae89aa66d
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-11-24 22:41:13 +0100

    created recipe model class and displaying a list of recipes in the recipes-list component (for not not using the recipes-list-item component)

--- src/app/recipes-panel/recipes-list/recipes-list.component.ts
+++ src/app/recipes-panel/recipes-list/recipes-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://images.pexels.com/photos/4099123/pexels-photo-4099123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ),
+    new Recipe(
+      'A test recipe 2',
+      'This is simply another test',
+      'https://images.pexels.com/photos/3872263/pexels-photo-3872263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
