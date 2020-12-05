50db5472bf4644142c55b3c60948ee7cd08ab331
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-05 18:59:22 +0100

    implemented a pseudo-navigation using ngIfs (as the routing hasn't been covered in the course yet)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public currentPseudoSelection = 'recipe';
+
+  public onPseudoNavigation(selectionName: string): void {
+    this.currentPseudoSelection = selectionName;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   public navbarCollapsed = true;
+  @Output() linkSelection = new EventEmitter<string>();
+
+  public onLinkSelected(selectionName: string): void {
+    this.linkSelection.emit(selectionName);
+  }
 }
