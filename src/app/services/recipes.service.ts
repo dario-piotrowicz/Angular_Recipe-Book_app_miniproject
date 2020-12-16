1660c2413890117a03612645ada6e3cddc4105d4
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-16 17:39:34 +0000

    added the ingredients fields in the recipe class and displaying those in the recipe-details component

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Ingredient } from '../models/ingredient.model';
 
 import { Recipe } from '../models/recipe.model';
 
@@ -12,14 +13,24 @@ export class RecipesService {
 
   private _recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is simply a test',
-      'https://images.pexels.com/photos/4099123/pexels-photo-4099123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      'Egg-Thinghy',
+      'Something done by mixing eggs and stuff',
+      'https://images.pexels.com/photos/4099123/pexels-photo-4099123.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      [
+        new Ingredient('eggs',4),
+        new Ingredient('glassfuls of milk', 1),
+        new Ingredient('glassfuls of flour', 1)
+      ]
     ),
     new Recipe(
-      'A test recipe 2',
-      'This is simply another test',
-      'https://images.pexels.com/photos/3872263/pexels-photo-3872263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      'Yogurt-Thingy',
+      'Some type of yogurt with berries and cereals mikes in it',
+      'https://images.pexels.com/photos/3872263/pexels-photo-3872263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      [
+        new Ingredient('glassfuls of yogurt',1),
+        new Ingredient('spoonfuls of dried berries', 3),
+        new Ingredient('spoonfuls of cereals', 2)
+      ]
     ),
   ];
 
