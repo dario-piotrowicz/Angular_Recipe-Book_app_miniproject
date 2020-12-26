1c7f462cff458eb3c9a8c6f8195333083ed0fde9
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-26 14:03:20 +0000

    dstinguish whether the recipe-editor is in edit or new mode

--- src/app/recipes-panel/recipe-editor/recipe-editor.component.ts
+++ src/app/recipes-panel/recipe-editor/recipe-editor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/models/recipe.model';
+
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-recipe-editor',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-editor.component.css']
 })
 export class RecipeEditorComponent implements OnInit {
+  public recipe: Recipe = null;
+  public editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private recipesService: RecipesService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe( params => {
+        const id = params['id'];
+        if(id){
+          this.recipe = this.recipesService.getRecipeById(params['id']);
+        } else {
+          this.recipe = null;
+        }
+        this.editMode = !!id;
+      }
+    )
   }
 
 }
