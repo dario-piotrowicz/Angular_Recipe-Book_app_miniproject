cc36a7daeb68e26f5892a07cfa01923e62bcabf3
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-01-06 17:07:50 +0000

    converting the shopping-list-editor's form into a proper Template Driven form

--- src/app/shoppig-list-panel/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/shoppig-list-panel/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/models/ingredient.model';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-editor',
   templateUrl: './shopping-list-editor.component.html',
-  styleUrls: ['./shopping-list-editor.component.css']
+  styleUrls: ['./shopping-list-editor.component.css'],
 })
 export class ShoppingListEditorComponent implements OnInit {
-  public newIngredientName: string;
-  public newIngredientAmount: number;
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  public addIngredient(): void {
-    const newIngredient = new Ingredient(this.newIngredientName, this.newIngredientAmount);
+  public addIngredient(form: NgForm): void {
+    const newIngredient = new Ingredient(form.value.name, form.value.amount);
     this.shoppingListService.addItemsToIngredientsList([newIngredient]);
-    this.newIngredientName = null;
-    this.newIngredientAmount = null;
+    form.reset();
   }
 }
