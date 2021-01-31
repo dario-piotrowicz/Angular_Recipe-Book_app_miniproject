c38f3647cc18c2d1c23d93107553a6a3446238b1
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-01-31 12:26:55 +0000

    fixed auth module issue

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AuthPageComponent } from './auth-page/auth-page.component';
 
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [AuthPageComponent, LoadingSpinnerComponent],
-  imports: [CommonModule, FormsModule],
+  declarations: [AuthPageComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
   exports: [AuthPageComponent],
 })
 export class AuthModule {}
