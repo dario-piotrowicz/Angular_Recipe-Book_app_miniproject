c95d5f6e7971164c26c904d5b0a037b24132e146
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-01-31 13:04:23 +0000

    moved the auth route to the auth-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthPageComponent } from './auth/auth-page/auth-page.component';
-
 const appRoutes: Routes = [
   {
     path: '',
     redirectTo: '/recipes',
     pathMatch: 'full',
-  },
-  {
-    path: 'auth',
-    component: AuthPageComponent,
   },
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AuthPageComponent } from './auth-page/auth-page.component';
 
+import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthPageComponent],
-  imports: [CommonModule, FormsModule, SharedModule],
+  imports: [CommonModule, FormsModule, SharedModule, AuthRoutingModule],
   exports: [AuthPageComponent],
 })
 export class AuthModule {}
