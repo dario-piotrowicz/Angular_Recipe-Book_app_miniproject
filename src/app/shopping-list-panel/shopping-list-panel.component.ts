f1fd6c20f25c482ae337aca5bdd806eca2bd911f
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-06 19:10:06 +0000

    bugfix, removing unnecessary unsubscription from shoppingListPanelComponent

--- src/app/shopping-list-panel/shopping-list-panel.component.ts
+++ src/app/shopping-list-panel/shopping-list-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { Ingredient } from '../models/ingredient.model';
 import { ShoppingListService } from '../services/shopping-list.service';
@@ -8,17 +8,12 @@ import { ShoppingListService } from '../services/shopping-list.service';
   templateUrl: './shopping-list-panel.component.html',
   styleUrls: ['./shopping-list-panel.component.css'],
 })
-export class shoppingListPanelComponent implements OnInit, OnDestroy {
+export class shoppingListPanelComponent implements OnInit {
   public ingredientsList$: Observable<Ingredient[]>;
-  private ingreditentsListChangeSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredientsList$ = this.shoppingListService.ingredientsList$;
-  }
-
-  ngOnDestroy(): void {
-    this.ingreditentsListChangeSubscription.unsubscribe();
   }
 }
